f2624954d619f4c1560d8a14e9a535ca93431ad5
Taichen <jatr812@gmail.com>
2021-03-18 14:19:13 -0700

    Got sessions and creating rooms semi working

--- music/api/views.py
+++ music/api/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format = None):
+        # If the user does not have a session, create one
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data = request.data)
+
+        # If the information given is valid
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host = host)
+
+            # If room has already been created, just update the room
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields = ['guest_can_pause', 'votes_to_skip'])
+            # Create new room
+            else:
+                room = Room(host=host, guest_can_pause = guest_can_pause, votes_to_skip = votes_to_skip)
+                room.save()
+
+            return Response(RoomSerializer(room).data)
